f8094f4117d0c945ae03f42c38d5690ca52c1905
Ekwenye Joseph <130204527+Joseph22-sys@users.noreply.github.com>
2025-06-13 05:20:19 +0100

    Update app.py to correct requests and remove radio btn

--- app.py
+++ app.py
@@ -10,16 +10,14 @@ st.title("Rack Data AI")
 
 st.session_state.url = st.text_input("Input a Website URL:")   
 
-    
-st.session_state.browser = st.radio(label="Select Your Browser",   options=["Google", "Edge"], horizontal=True)
-
+ 
 payload = { 'api_key': st.secrets["SCRAPER_API_KEY"], 'url': st.session_state.url, 'output_format': 'text', 'device_type': 'desktop' }
 
 
 if st.button("Scrape/Analyze"):
     st.write("Scraping the site...")
     try:
-        page_content = request.get('https://api.scraperapi.com/', params=payload).text
+        page_content = requests.get('https://api.scraperapi.com/', params=payload).text
         
         
         content = output_data(page_content)
